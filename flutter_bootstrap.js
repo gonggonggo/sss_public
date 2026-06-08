186649e8e3ae4a61e4574ba6ea677a770b709758
gonggonggo <yano.go.mail@gmail.com>
2026-06-08 20:43:24 +0900

    送り仮名追加と設計書更新

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3646026677" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3109804902" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
