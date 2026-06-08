a44da86c4b63b71ffe46470b4e6ca97efca596c5
gonggonggo <yano.go.mail@gmail.com>
2026-06-08 21:20:43 +0900

    漢字読みの正誤判定再実装

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1241333763" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "26182712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
